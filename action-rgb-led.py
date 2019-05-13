9487e602e0f3aa04e296dd0072d4259faddb5a94
tierlord <xtra-j@web.de>
2019-05-13 10:49:10 +0200

    Update action-rgb-led.py

--- action-rgb-led.py
+++ action-rgb-led.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-
 import apa102
 import time
 import paho.mqtt.client as mqtt
 from threading import Thread
 
 led = apa102.APA102(num_led=3)
+hotword_active = True
+stop_thread = False
+thread_running = False
 
 def set_led(color):
     led.set_pixel(0, color[0], color[1], color[2])
@@ -15,6 +17,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected")
     client.subscribe("hermes/hotword/#")
     client.subscribe("hermes/tts/#")
+    client.subscribe("snips/led/#")
     print("Callbacks added")
 
 def on_message(client, userdata, msg):
@@ -22,16 +25,24 @@ def on_message(client, userdata, msg):
 
 def hotword_on(client, userdata, msg):
     for i in range(51):
-        set_led([0,255-51*5,0])
+        set_led([0,255-i*5,0])
         time.sleep(0.005)
+    global hotword_active
+    hotword_active = True
+    print("Hotword on")
 
 def hotword_off(client, userdata, msg):
-    for times in range(3):
+    for times in range(2):
         for i in range(51):
             set_led([0,i*5,0])
-            time.sleep(0.004)
+            time.sleep(0.001)
+            if times == 1:
+                time.sleep(0.005)
         set_led([0,0,0])
     set_led([0,255,0])
+    global hotword_active
+    hotword_active = False
+    print("Hotword off")
 
 def rainbow():
     global speaking_bool
@@ -59,6 +70,71 @@ def rainbow():
     print("Rainbow ended")
     set_led([0,0,0])
 
+def led_an_thread(client, userdata, msg):
+    global speaking_bool
+    global hotword_active
+    global stop_thread
+    global thread_running
+    while speaking_bool or not hotword_active or thread_running:
+        stop_thread = True
+        time.sleep(0.1) # wait
+    stop_thread = False
+    thread_running = True
+    c = [0,255,0]
+    while c[0] < 255 and not stop_thread:
+        set_led(c)
+        c[0] += 5
+        c[2] += 5
+        time.sleep(0.01)
+    
+    try:
+        load = msg.payload.decode("utf-8")
+        if load != "":
+            secs = float(load) * 60
+            start_time = time.time()
+            while c[0] > 0 and not stop_thread:
+                if time.time() - start_time < secs - 30:
+                    time.sleep(1)
+                else:
+                    set_led(c)
+                    c[0] -= 1
+                    if c[1] >= 2:
+                        c[1] -= 2
+                    if c[2] >= 2:
+                        c[2] -= 2
+                    time.sleep(0.115)
+            c = [0,0,0]
+            set_led(c)
+    except:
+        pass
+    thread_running = False
+
+def led_aus_thread(client, userdata, msg):
+    global speaking_bool
+    global hotword_active
+    global stop_thread
+    global thread_running
+    while speaking_bool or not hotword_active or thread_running:
+        stop_thread = True
+        time.sleep(0.1) # wait
+    stop_thread = False
+    thread_running = True
+    c = [255,255,255]
+    while c[0] > 0 and not stop_thread:
+        set_led(c)
+        for i in range(3):
+            c[i] -= 1
+        time.sleep(0.03)
+    thread_running = False
+
+def led_an(client, userdata, msg):
+    t = Thread(target=led_an_thread, args=(client,userdata,msg,))
+    t.start()
+
+def led_aus(client, userdata, msg):
+    t = Thread(target=led_aus_thread, args=(client,userdata,msg,))
+    t.start()
+
 speaking_bool = False
 def speaking(client, userdata, msg):
     global speaking_bool
@@ -78,6 +154,8 @@ client.message_callback_add("hermes/hotword/toggleOn", hotword_on)
 client.message_callback_add("hermes/hotword/toggleOff", hotword_off)
 client.message_callback_add("hermes/tts/say", speaking)
 client.message_callback_add("hermes/tts/sayFinished", speaking_stop)
+client.message_callback_add("snips/led/an", led_an)
+client.message_callback_add("snips/led/aus", led_aus)
 client.connect("localhost", 1883, 60)
 set_led([0,0,0])
 client.loop_forever()
